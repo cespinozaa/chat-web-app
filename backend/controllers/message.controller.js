8f89ece55e70f7af4ed199d6a18ba131711a38e5
Cynthia Espinoza-Arredondo <c_espinozaa@uncg.edu>
2024-11-26 19:10:06 -0500

    completed socket

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
     try {
@@ -27,10 +28,15 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         }
 
-        //socket io functionality will go here
-
         //runs in parallel
         await Promise.all([conversation.save(), newMessage.save()]);
+
+        //socket io functionality
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId) {
+            //io.to(<socket_id>).emit() is used to send events to a specific client
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(201).json(newMessage);
 
